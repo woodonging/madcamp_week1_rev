83b17c0fab36c876000ba8e857eaaa2025266ec1
udong1 <dngus1082@gmail.com>
2023-12-30 10:10:35 +0900

    Contact recyclerview구현

--- app/src/main/java/com/example/madcamp_week1_rev/ContactFragment.kt
+++ app/src/main/java/com/example/madcamp_week1_rev/ContactFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ContactFragment : Fragment() {
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact, container, false)
+        val inflatedView = inflater.inflate(R.layout.fragment_contact, container, false)
+        val contacts = inflatedView.findViewById<RecyclerView>(R.id.contact_board)
+        val contactList = ArrayList<Contact>()
+
+        contactList.add(Contact("a"))
+        contactList.add(Contact("b"))
+        contactList.add(Contact("c"))
+        contactList.add(Contact("d"))
+
+
+        val contactAdapter = ContactAdapter(contactList)
+
+        contactAdapter.notifyDataSetChanged()
+        contacts.adapter = contactAdapter
+        contacts.layoutManager = LinearLayoutManager(this.context)
+
+        return inflatedView
     }
 
 }
