fdada3772b3c93aa3d08e551b606c76a9482021d
udong1 <dngus1082@gmail.com>
2024-01-03 15:05:13 +0900

    날씨탭 이미지 추가 및 UI 작업 추가 탭 구현중

--- app/src/main/java/com/example/madcamp_week1_rev/WeatherData.kt
+++ app/src/main/java/com/example/madcamp_week1_rev/WeatherData.kt
@@ -1,16 +1,20 @@
 package com.example.madcamp_week1_rev
 
-import android.location.Geocoder
 import org.json.JSONException
 import org.json.JSONObject
+import kotlin.random.Random
 
 class WeatherData{
 
     lateinit var tempString: String
     lateinit var tempmaxString: String
     lateinit var tempminString: String
+    lateinit var weatherDescription: String
     lateinit var icon: String
     lateinit var weatherType: String
+    lateinit var humidity : String
+    lateinit var feels_like : String
+    lateinit var windspeed : String
     lateinit var latstring: String
     lateinit var lonstring: String
     private var weatherId: Int = 0
@@ -21,6 +25,11 @@ class WeatherData{
             weatherData.weatherId = jsonObject?.getJSONArray("weather")?.getJSONObject(0)?.getInt("id")!!
             weatherData.weatherType = jsonObject.getJSONArray("weather").getJSONObject(0).getString("main")
             weatherData.icon = updateWeatherIcon(weatherData.weatherId)
+            weatherData.weatherDescription = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description")
+            weatherData.humidity = jsonObject.getJSONObject("main").getString("humidity")
+            weatherData.feels_like = jsonObject.getJSONObject("main").getString("feels_like")
+            weatherData.windspeed = jsonObject.getJSONObject("wind").getString("speed")
+
             val lat: Double = (jsonObject.getJSONObject("coord").getDouble("lat"))
             val lon: Double = (jsonObject.getJSONObject("coord").getDouble("lon"))
             val roundedTemp: Int = (jsonObject.getJSONObject("main").getDouble("temp")-273.15).toInt()
@@ -40,35 +49,63 @@ class WeatherData{
     }
 
     private fun updateWeatherIcon(condition: Int): String {
-        if (condition in 200..299) {
-            return "weather_thunderstorm"
-        } else if (condition in 300..499) {
-            return "wp_rain" //lightrain
-        } else if (condition in 500..599) {
-            return "wp_rain"
-        } else if (condition in 600..700) {
-            return "wp_snow"
-        } else if (condition in 701..771) {
-            return "weather_fog"
-        } else if (condition in 772..799) {
-            return "wp_cloudy"
-        } else if (condition == 800) {
-            return "wp_sunny"
-        } else if (condition in 801..804) {
-            return "wp_cloudy"
-        } else if (condition in 900..902) {
-            return "weather_thunderstorm"
+        when (condition) {
+            in 200..299 -> {
+                return "weather_thunderstorm"
+            }
+            in 300..499 -> {
+                return "weather_rain" //lightrain
+            }
+            in 500..599 -> {
+                return "weather_rain"
+            }
+            in 600..700 -> {
+                return "weather_snow"
+            }
+            in 701..771 -> {
+                return "weather_fog"
+            }
+            in 772..799 -> {
+                return "weather_cloudy"
+            }
+            800 -> {
+                return "weather_sunny"
+            }
+            in 801..804 -> {
+                return "weather_cloudy"
+            }
+            in 900..902 -> {
+                return "weather_thunderstorm"
+            }
+            903 -> {
+                return "weather_snow"
+            }
+            904 -> {
+                return "weather_sunny"
+            }
+            else -> return if (condition in 905..1000) {
+                "weather_thunderstorm"
+            } else "dunno"
         }
-        if (condition == 903) {
-            return "wp_snow"
-        }
-        if (condition == 904) {
-            return "wp_sunny"
-        }
-        return if (condition in 905..1000) {
-            "weather_thunderstorm"
-        } else "dunno"
 
+    }
+
+    fun updateWeatherImage(time: Int): String {
+        val number = Random.nextInt(5)
+        val timer = when (time) {
+                in 6 .. 9 -> {
+                    "morning"
+                }
+                in 10 .. 17 -> {
+                    "noon"
+                }
+                in 18 .. 21 -> {
+                    "evening"
+                }
+                else -> "night"
+        }
+//        return "$timer$number"
+        return "${timer}${number}"
     }
 
 }
